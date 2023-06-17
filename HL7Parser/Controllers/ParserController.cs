afca0810f155cc1811d3e6d2ac58306d65636ec5
shree256 <shreeshan256@gmail.com>
2023-06-17 20:19:46 +0530

    Adds new models and Updated controller

---
+++ HL7Parser/Controllers/ParserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.Json;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NHapi.Base.Parser;
+using NHapi.Model.V23.Message;
+
+namespace HL7Parser.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HL7ParserController : ControllerBase
+{
+    //static ParsedMessageViewModel messageToParse = new ParsedMessageViewModel();
+
+    [HttpPost]
+    public IActionResult HL7MessageConverter([FromBody] HL7Message payload)
+    {
+        const string hl7Message = "MSH|^~\\&|SUNS1|OVI02|AZIS|CMD|200606221348||ADT^A01|1049691900|P|2.3\r"
+            + "EVN|A01|200803051509||||200803031508\r"
+            + "PID|||5520255^^^PK^PK~ZZZZZZ83M64Z148R^^^CF^CF~ZZZZZZ83M64Z148R^^^SSN^SSN^^20070103^99991231~^^^^TEAM||ZZZ^ZZZ||19830824|F||||||||||||||||||||||N\r"
+            + "ZPV|Some Custom Notes|Additional custom description of the visit goes here";
+
+        var parser = new PipeParser();
+
+        try
+        {
+            var parsedMessage = parser.Parse(hl7Message, "2.3");
+            var messageHeader = new ADTA01MessageHeader();
+            var adtParsedMessage = new ADTA01ParsedMessage();
+
+            var adtMessage = parsedMessage as NHapi.Model.V23.Message.ADT_A01;
+            var msh = adtMessage.MSH;
+
+            messageHeader.App = msh.SendingApplication.NamespaceID.Value;
+            messageHeader.ControlId = msh.MessageControlID.Value;
+            messageHeader.Facility = msh.SendingFacility.NamespaceID.Value;
+            messageHeader.Type = msh.MessageType.MessageType.Value;
+            messageHeader.Version = msh.VersionID.Value;
+            messageHeader.MsgTime = msh.DateTimeOfMessage.TimeOfAnEvent.Value;
+
+            adtParsedMessage.MessageHeader = messageHeader;
+
+            string strJson = JsonSerializer.Serialize<ADTA01ParsedMessage>(adtParsedMessage);
+            return Ok(strJson);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error occured -> {e.StackTrace}");
+        }
+
+        return Ok("Parse Failed");
+    }
+}
+
---
+++ HL7Parser/Models/ParserModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using NHapi.Model.V23.Message;
+using NHapi.Model.V23.Segment;
+
+namespace HL7Parser;
+
+public class HL7Message
+{
+    public string? Message { get; set; }
+}
+
+public class ParsedADTA01Model
+{
+    public MSH MSHSegment { get; set; }
+    public PID PIDSegment { get; set; }
+}
+
+public class ADTA01MessageHeader
+{
+    public string App { get; set; }
+    public string Facility { get; set; }
+    public string MsgTime { get; set; }
+    public string ControlId { get; set; }
+    public string Type { get; set; }
+    public string Version { get; set; }
+}
+
+public class ADTA01ParsedMessage
+{
+    public ADTA01MessageHeader MessageHeader { get; set; }
+}
